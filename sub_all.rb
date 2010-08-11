399731190f7c30e8fba28f4b5b6089c091f862f1
Sho Hashimoto <hashimoto@shokai.org>
2010-08-11 12:46:44 +0900

    sub all

---
+++ sub_all.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'zmq'
+
+ctx = ZMQ::Context.new
+sock= ctx.socket(ZMQ::SUB)
+sock.connect('tcp://127.0.0.1:5000')
+sock.setsockopt(ZMQ::SUBSCRIBE, '') # all
+
+loop do
+  puts sock.recv()
+end
