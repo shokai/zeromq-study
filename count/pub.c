e6f89917c24ccc823a47a0dd160a2bfe5dc820df
Sho Hashimoto <hashimoto@shokai.org>
2010-08-11 18:49:53 +0900

    assert pub.c

--- count/pub.c
+++ count/pub.c
@@ -2,14 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <zmq.h>
+#include <assert.h>
 
 int main(int argc, char **argv){
   printf("pub start\n");
+  int rc;
   void *ctx, *sock;
   zmq_msg_t msg;
-  ctx = zmq_init(1);
-  sock = zmq_socket (ctx, ZMQ_PUB);
-  zmq_bind(sock, "tcp://127.0.0.1:5000");
+  assert(ctx = zmq_init(1));
+  assert(sock = zmq_socket(ctx, ZMQ_PUB));
+  rc = zmq_bind(sock, "tcp://127.0.0.1:5000");
+  assert(rc == 0);
 
   int count;
   count = 0;
@@ -19,10 +22,16 @@ int main(int argc, char **argv){
     sprintf(m, "count %d", count);
     printf("%s\n", m);
     
-    zmq_msg_init(&msg);
+    rc = zmq_msg_init(&msg);
+    assert(rc == 0);
+
     zmq_msg_init_size(&msg, strlen(m)+1);
+    assert(rc == 0);
+
     memcpy(zmq_msg_data(&msg), m, strlen(m)+1);
-    zmq_send (sock, &msg, 0);
+    zmq_send(sock, &msg, 0);
+    assert(rc == 0);
+
     count++;
     sleep(1);
   }
